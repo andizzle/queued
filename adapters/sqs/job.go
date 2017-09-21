cb1a135aa025b051b100d9209a948304fb51af70
Andy Zhang <andizzle.zhang@gmail.com>
2017-09-22 00:48:59 +1000

    add in checksum

--- adapters/sqs/job.go
+++ adapters/sqs/job.go
@@ -1,6 +1,11 @@
 package sqs
 
-import "github.com/aws/aws-sdk-go/service/sqs"
+import (
+	"crypto/md5"
+	"encoding/hex"
+
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
 
 // Job that holds sqs message detail
 type Job struct {
@@ -13,4 +18,10 @@ func (j Job) GetJobID() *string {
 
 func (j Job) GetBody() *string {
 	return j.Body
+}
+
+func (j Job) CheckSum() bool {
+	sum := md5.Sum([]byte(*j.Body))
+
+	return hex.EncodeToString(sum[:]) == *j.MD5OfBody
 }
